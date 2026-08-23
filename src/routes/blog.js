55acb6614d62a95755bc3d8318aa167d4d66ffed
Your Name <you@example.com>
2026-08-23 16:19:04 +0700

    up

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,43 +1,39 @@
-
 const express = require('express');
+
 const router = express.Router();
 
 const BlogController = require('../app/controllers/BlogController');
+
 const HomeController = require('../app/controllers/HomeController');
 
 
 // ========================================
 // TRANG TẠO BÀI VIẾT
 // ========================================
-
 router.get('/create', HomeController.create);
 
 
 // ========================================
 // LƯU BÀI VIẾT
 // ========================================
-
 router.post('/create', HomeController.store);
 
 
 // ========================================
 // TRANG SỬA BÀI VIẾT
 // ========================================
-
 router.get('/:id/edit', HomeController.edit);
 
 
 // ========================================
 // CẬP NHẬT BÀI VIẾT
 // ========================================
-
 router.put('/:id', HomeController.update);
 
 
 // ========================================
 // XÓA BÀI VIẾT
 // ========================================
-
 router.delete('/:id', HomeController.delete);
 
 
@@ -45,8 +41,7 @@ router.delete('/:id', HomeController.delete);
 // CHI TIẾT BÀI VIẾT
 // ĐỂ CUỐI CÙNG
 // ========================================
-
-router.get('/:slug', BlogController.show);
+router.get('/:slug', HomeController.show);
 
 
 module.exports = router;
